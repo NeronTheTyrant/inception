5d357990fe4cc63a7711ace8370e9abc971f22d2
Martin <mlebard@debian.biteof1987.com>
2022-06-13 14:26:14 -0400

    ssl certificate is sort of working now

--- Makefile
+++ Makefile
@@ -16,24 +16,24 @@ up:
 	@echo "Building containers ..."
 	@docker compose -f $(COMPOSE) up -d --build
 
-up: 
-	@echo "Building volume directories ..."
-	@sudo mkdir -p /home/mlebard/data/wordpress
-	@sudo mkdir -p /home/mlebard/data/mysql
-	@echo "Building containers ..."
-	@docker compose -f $(COMPOSE) --verbose up
+#up: 
+#	@echo "Building volume directories ..."
+#	@sudo mkdir -p /home/mlebard/data/wordpress
+#	@sudo mkdir -p /home/mlebard/data/mysql
+#	@echo "Building containers ..."
+#	@docker compose -f $(COMPOSE) --verbose up
 
 clean: 	
 	@echo "Stopping containers ..."
 	@docker compose -f $(COMPOSE) down
-	@docker stop `docker ps -qa`
-	@docker rm `docker ps -qa`
+	@docker container prune 
+#	@docker rm `docker ps -qa`
 	@echo "Deleting all images ..."
 	@docker rm -f `docker images -qa`
 	@echo "Deleting all volumes ..."
-	@docker volume rm `docker volume ls -q`
+	@docker volume prune
 	@echo "Deleting all network ..."
-	@docker network rm `docker network ls -q`
+	@docker network prune
 	@echo "Deleting all data ..."
 	@sudo rm -rf /home/mlebard/data/wordpress
 	@sudo rm -rf /home/mlebard/data/mysql
--- srcs/req/mariadb/tools/create_db.sh
+++ srcs/req/mariadb/tools/create_db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-echo "entrypoint start"
+
 mysql_install_db
-echo "after install"
+
 /etc/init.d/mysql start
 
 #Check if the database exists
@@ -14,32 +14,29 @@ else
 
 # Set root option so that connexion without root password is not possible
 
-mysql_secure_installation <<_EOF_
-Y
-$MYSQL_ROOT_PASSWORD
-$MYSQL_ROOT_PASSWORD
-Y
-n
-Y
-Y
+#Set root user password
+mysql --user=root -p <<_EOF_
+
+UPDATE mysql.user SET Password=('$MYSQL_ROOT_PASSWORD') WHERE User='root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
+FLUSH PRIVILEGES;
 _EOF_
 
 #Add a root user on 127.0.0.1 to allow remote connexion
 
-	echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+#	echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
 #Create database and user for wordpress
 	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
 #Import database
-mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+#mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
 
 fi
 
-echo "after fi"
-
 /etc/init.d/mysql stop
-
-echo "6"
 
 exec "$@"
