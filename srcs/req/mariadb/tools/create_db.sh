ace4f4507e2b0d0c0243aff501814f17f791a11c
mlebard <mlebard@debian.mlebard.42.fr>
2022-07-14 10:17:13 -0400

    fixed a bunch of issues in the mariadb script, every command in the heredoc were not getting through. The solution was to use normal piped commands, change the login plugin for the root user, and change the default password in the debian.cnf file generated in /etc/mysql/ . I will never set up a root password ever again in mysql, holy shit.

--- srcs/req/mariadb/tools/create_db.sh
+++ srcs/req/mariadb/tools/create_db.sh
@@ -15,28 +15,40 @@ else
 # Set root option so that connexion without root password is not possible
 
 #Set root user password
-mysql --user=root -p <<_EOF_
+#mysql --user=root -p << EOF
+#
+#UPDATE mysql.user SET Password=("$MYSQL_ROOT_PASSWORD") WHERE User='root';
+#UPDATE mysql.user SET Plugin='mysql_native_password' WHERE User='root'
+#DELETE FROM mysql.user WHERE User='';
+#DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+#DROP DATABASE test;
+#DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
+#FLUSH PRIVILEGES;
+#EOF
 
-UPDATE mysql.user SET Password=('$MYSQL_ROOT_PASSWORD') WHERE User='root';
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
-FLUSH PRIVILEGES;
-_EOF_
+	echo "SET PASSWORD FOR 'debian-sys-maint'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD'); FLUSH PRIVILEGES;" | mysql -uroot
+	echo "SET PASSWORD FOR 'debian-sys-maint'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD'); FLUSH PRIVILEGES;"
+
+	echo "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password; ALTER USER root@localhost IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+	echo "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password; ALTER USER root@localhost IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
+
+#	echo "DROP DATABASE test; FLUSH PRIVILEGES;" | mysql -uroot -p$MYSQL_ROOT_PASSWORD	
+#	echo "DROP DATABASE test; FLUSH PRIVILEGES;"	
 
 #Add a root user on 127.0.0.1 to allow remote connexion
 
 #	echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
 #Create database and user for wordpress
-	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
+	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;"
 
 #Import database
 #mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
 
+	sed -i "s/password = /password = $MYSQL_ROOT_PASSWORD/g" /etc/mysql/debian.cnf
 fi
 
 /etc/init.d/mysql stop
-
+echo "this is the end"
 exec "$@"
